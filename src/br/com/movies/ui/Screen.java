02f9acb6c9a8769e66d7a44c72e2f2115f6fa352
Aristofany Herderson <programmeraristofanyherderson@gmail.com>
2024-11-24 19:14:34 -0300

    :sparkles: feature: add update, delete and clear option on GUI

--- src/br/com/movies/ui/Screen.java
+++ src/br/com/movies/ui/Screen.java
@@ -47,8 +47,9 @@ public final class Screen extends javax.swing.JPanel {
 
         for (Movie movie : movies) {
             ArrayList<Object> rowData = new ArrayList<>();
+            rowData.add(movie.getId());
             rowData.add(movie.getTitle());
-            rowData.add(movie.getDuration());
+            rowData.add(movie.getDuration() + " minutos");
             rowData.add(movie.getAgeRating());
 
             dtm.addRow(rowData.toArray());
@@ -60,6 +61,29 @@ public final class Screen extends javax.swing.JPanel {
 
         List<Movie> movies = dao.getMovies();
         loadAllDataIntoTable(movies);
+    }
+
+    public Movie returnDataToTextFields(int id) {
+        Movie movie = null;
+
+        try {
+            MovieDAO dao = new MovieDAO();
+            movie = dao.getMovieById(id);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "" + e);
+        }
+
+        return movie;
+    }
+
+    public void clearFields() {
+        id = 0;
+        title.setText("");
+        release.setText("");
+        rating.setSelectedIndex(1);
+        duration.setText("");
+        trailer.setText("");
+        image.setText("");
     }
 
     /**
@@ -86,10 +110,13 @@ public final class Screen extends javax.swing.JPanel {
         jLabel8 = new javax.swing.JLabel();
         image = new javax.swing.JTextField();
         jLabel9 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        Update = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         table = new javax.swing.JTable();
         jLabel10 = new javax.swing.JLabel();
+        Clear = new javax.swing.JButton();
+        Add = new javax.swing.JButton();
+        Delete = new javax.swing.JButton();
 
         jLabel1.setText("jLabel1");
 
@@ -193,17 +220,17 @@ public final class Screen extends javax.swing.JPanel {
         jLabel9.setText("Imagem URL");
         add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(31, 272, -1, -1));
 
-        jButton1.setBackground(new java.awt.Color(19, 20, 22));
-        jButton1.setFont(new java.awt.Font("Montserrat SemiBold", 0, 12)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("Adicionar");
-        jButton1.setFocusable(false);
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        Update.setBackground(new java.awt.Color(19, 20, 22));
+        Update.setFont(new java.awt.Font("Montserrat SemiBold", 0, 12)); // NOI18N
+        Update.setForeground(new java.awt.Color(255, 255, 255));
+        Update.setText("Atualizar");
+        Update.setFocusable(false);
+        Update.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                UpdateActionPerformed(evt);
             }
         });
-        add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(31, 334, 346, 32));
+        add(Update, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 334, 167, 32));
 
         table.setFont(new java.awt.Font("Montserrat Medium", 0, 12)); // NOI18N
         table.setForeground(new java.awt.Color(13, 14, 15));
@@ -212,14 +239,14 @@ public final class Screen extends javax.swing.JPanel {
 
             },
             new String [] {
-                "Título", "Duração", "Classificação"
+                "id", "Título", "Duração", "Classificação"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.String.class, java.lang.Integer.class, java.lang.String.class
+                java.lang.Integer.class, java.lang.String.class, java.lang.Integer.class, java.lang.String.class
             };
             boolean[] canEdit = new boolean [] {
-                false, false, false
+                false, false, false, false
             };
 
             public Class getColumnClass(int columnIndex) {
@@ -232,17 +259,20 @@ public final class Screen extends javax.swing.JPanel {
         });
         table.setToolTipText("");
         table.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        table.setFocusable(false);
         table.setGridColor(new java.awt.Color(204, 204, 204));
         table.setIntercellSpacing(new java.awt.Dimension(6, 6));
         table.setName(""); // NOI18N
         table.setRequestFocusEnabled(false);
         table.setRowHeight(30);
-        table.setRowSelectionAllowed(false);
         table.setShowGrid(false);
         table.getTableHeader().setReorderingAllowed(false);
         table.setUpdateSelectionOnSort(false);
         table.setVerifyInputWhenFocusTarget(false);
+        table.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tableMouseClicked(evt);
+            }
+        });
         jScrollPane1.setViewportView(table);
 
         add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(432, 108, 484, -1));
@@ -250,6 +280,42 @@ public final class Screen extends javax.swing.JPanel {
         jLabel10.setFont(new java.awt.Font("Montserrat Medium", 0, 14)); // NOI18N
         jLabel10.setText("Filmes já adicionados");
         add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(432, 62, -1, -1));
+
+        Clear.setBackground(new java.awt.Color(19, 20, 22));
+        Clear.setFont(new java.awt.Font("Montserrat SemiBold", 0, 12)); // NOI18N
+        Clear.setForeground(new java.awt.Color(255, 255, 255));
+        Clear.setText("Limpar");
+        Clear.setFocusable(false);
+        Clear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ClearActionPerformed(evt);
+            }
+        });
+        add(Clear, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 370, 167, 32));
+
+        Add.setBackground(new java.awt.Color(19, 20, 22));
+        Add.setFont(new java.awt.Font("Montserrat SemiBold", 0, 12)); // NOI18N
+        Add.setForeground(new java.awt.Color(255, 255, 255));
+        Add.setText("Adicionar");
+        Add.setFocusable(false);
+        Add.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AddActionPerformed(evt);
+            }
+        });
+        add(Add, new org.netbeans.lib.awtextra.AbsoluteConstraints(31, 334, 167, 32));
+
+        Delete.setBackground(new java.awt.Color(19, 20, 22));
+        Delete.setFont(new java.awt.Font("Montserrat SemiBold", 0, 12)); // NOI18N
+        Delete.setForeground(new java.awt.Color(255, 255, 255));
+        Delete.setText("Deletar");
+        Delete.setFocusable(false);
+        Delete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                DeleteActionPerformed(evt);
+            }
+        });
+        add(Delete, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 370, 167, 32));
     }// </editor-fold>//GEN-END:initComponents
 
     private void titleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_titleActionPerformed
@@ -272,7 +338,34 @@ public final class Screen extends javax.swing.JPanel {
         // TODO add your handling code here:
     }//GEN-LAST:event_imageActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void UpdateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_UpdateActionPerformed
+        String movieTitle = title.getText().trim();
+        int movieRelease = Integer.parseInt(release.getText().trim());
+        int movieDuration = Integer.parseInt(duration.getText().trim());
+        String movieRating = (String) rating.getSelectedItem();
+        String movieTrailer = trailer.getText().trim();
+        String movieImage = image.getText().trim();
+
+        if (movieTitle.isEmpty() || String.valueOf(movieRelease).trim().isEmpty() || String.valueOf(movieDuration).trim().isEmpty() || movieRating.isEmpty() || movieTrailer.isEmpty() || movieImage.isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Por favor, preencha todos os campos obrigatórios.", "Erro", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        Movie movie = new Movie(movieDuration, movieTitle, movieRelease, movieRating, movieTrailer, movieImage);
+        MovieDAO dao = new MovieDAO();
+
+        dao.update(movie, id);
+        loadTable();
+    }//GEN-LAST:event_UpdateActionPerformed
+
+    private void ratingActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ratingActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ratingActionPerformed
+
+    private void ClearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ClearActionPerformed
+        clearFields();
+    }//GEN-LAST:event_ClearActionPerformed
+
+    private void AddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AddActionPerformed
         String movieTitle = title.getText().trim();
         String movieRelease = release.getText().trim();
         String movieDuration = duration.getText().trim();
@@ -286,12 +379,12 @@ public final class Screen extends javax.swing.JPanel {
         }
 
         try {
-    
+
             int durationValue = Integer.parseInt(movieDuration);
 
             Movie newMovie = new Movie();
             newMovie.setTitle(movieTitle);
-            newMovie.setReleaseYear(Integer.parseInt(movieRelease)); 
+            newMovie.setReleaseYear(Integer.parseInt(movieRelease));
             newMovie.setDuration(durationValue);
             newMovie.setAgeRating(movieRating);
             newMovie.setTrailerUrl(movieTrailer);
@@ -301,30 +394,50 @@ public final class Screen extends javax.swing.JPanel {
             dao.add(newMovie);
 
             loadTable();
+            clearFields();
             
-            title.setText("");
-            release.setText("");
-            duration.setText("");
-            trailer.setText("");
-            image.setText("");
-
             JOptionPane.showMessageDialog(this, "Filme adicionado com sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
         } catch (NumberFormatException ex) {
             JOptionPane.showMessageDialog(this, "A duração deve ser um número válido.", "Erro", JOptionPane.ERROR_MESSAGE);
         } catch (HeadlessException ex) {
             JOptionPane.showMessageDialog(this, "Erro ao salvar o filme: " + ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
         }
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_AddActionPerformed
 
-    private void ratingActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ratingActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_ratingActionPerformed
+    private void DeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DeleteActionPerformed
+        MovieDAO dao = new MovieDAO();
+
+        dao.delete(id);
+        clearFields();
+        loadTable();
+    }//GEN-LAST:event_DeleteActionPerformed
+    int id = 0;
+    private void tableMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tableMouseClicked
+        int selectedRow = table.getSelectedRow();
+
+        if (selectedRow < 0) {
+            JOptionPane.showMessageDialog(null, "Por favor, selecione algum filme para atualização");
+        } else {
+            id = (int) table.getValueAt(selectedRow, 0);
+            Movie movie = returnDataToTextFields(id);
+
+            title.setText(movie.getTitle());
+            release.setText(String.valueOf(movie.getReleaseYear()));
+            duration.setText(String.valueOf(movie.getDuration()));
+            rating.setSelectedItem(movie.getAgeRating());
+            trailer.setText(movie.getTrailerUrl());
+            image.setText(movie.getImageUrl());
+        }
+    }//GEN-LAST:event_tableMouseClicked
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Add;
+    private javax.swing.JButton Clear;
+    private javax.swing.JButton Delete;
+    private javax.swing.JButton Update;
     private javax.swing.JTextField duration;
     private javax.swing.JTextField image;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
